82a80ec9eb19ca16299cdf11846d7ae88102afad
OD1995 <oliverdernie1@googlemail.com>
2021-01-15 10:12:28 +0000

    still not working

--- CreateChunks/__init__.py
+++ CreateChunks/__init__.py
@@ -16,6 +16,8 @@ from MyFunctions import get_SAS_URL
 import tempfile
 from urllib.parse import unquote
 import json
+import ffmpy
+import subprocess
 
 def main(inputs: str) -> str:
     ## Arguments
@@ -23,7 +25,10 @@ def main(inputs: str) -> str:
     logging.info(f"fileType: {fileType}")
     logging.info(f"fileURL: {fileURL}")
     logging.info(f"container: {container}")
-    logging.info("changes 10")
+    logging.info("changes 11")
+    ## Set file name to be used and get container it came from
+    fileName = unquote(fileURL.split("/")[-1]) # (contains file extension)
+    container = fileURL.split("/")[-2]
     ## Create bbs
     bbs = BlockBlobService(connection_string=os.getenv("fsevideosConnectionString"))
     ## Create SAS URL
@@ -33,58 +38,70 @@ def main(inputs: str) -> str:
         container=container
     )
     logging.info(f"sasURL: {sasURL}")
+    ## Download blob to temporary location
+    tempClipFilePath = "/tmp/" + fileName
+    bbs.get_blob_to_path(
+        container_name=container,
+        blob_name=fileName,
+        file_path=tempClipFilePath
+    )
+    logging.info(f'file saved to "{tempClipFilePath}"')
     ## Set the size of the clips you want (in seconds)
     chunk_length_secs = 3600
-    ## Set file name to be used
-    fileName = unquote(fileURL.split("/")[-1])
     ## Create clip
     if fileType == "MP4":
-        clip = VideoFileClip(sasURL)
+        clip = VideoFileClip(tempClipFilePath)
     elif fileType == "MP3":
-        clip = AudioFileClip(sasURL)
+        clip = AudioFileClip(tempClipFilePath)
     else:
         raise ValueError("wrong file type")
-    ## Get file extension
-    fileExtension = fileURL.split(".")[-1]
     ## Get number of chunks (files to be created)
+    logging.info(f"clip.duration: {clip.duration}")
     chunk_count = ceil(clip.duration / chunk_length_secs)
     ## Loop through the chunks
     for a in range(chunk_count):
-        ## Create a string to add to the front of the file name for this chunk
-        filePrefix = f"{a+1}of{chunk_count}"
-        ## If it's the last subclip, do last stopping point until end
-        if a == chunk_count - 1:
-            subclip = clip.subclip(
-                            t_start=a*chunk_length_secs,
-                            t_end=clip.duration
-                            )
-        ## Otherwise just do the next `chunk_length_secs`
-        else:
-            subclip = clip.subclip(
-                            t_start=a*chunk_length_secs,
-                            t_end=(a+1)*chunk_length_secs
-                            )
-        logging.info(f"File: {filePrefix}")
-        logging.info(f"Duration: {subclip.duration} seconds")
+        ## Get prefix to use
+        subclipPrefix = f"{a+1}of{chunk_count}"
+        subclipFileName = f"{subclipPrefix}_{fileName}"
+        logging.info(f"clip: {subclipPrefix}")
         A = datetime.now()
-        ## Download locally to temporary then upload to Azure
-        # with tempfile.gettempdir() as dirpath:
-        fn = f"{filePrefix}_{fileName}"
-        tempFilePath = "/tmp/" + fn
-        # tempFilePath = tempfile.NamedTemporaryFile(suffix=f".{fileExtension}").name
-        logging.info(f"tempFilePath: {tempFilePath}")
-        if fileType == "MP4":
-            subclip.write_videofile(filename=tempFilePath)
-        elif fileType == "MP3":
-            subclip.write_audiofile(filename=tempFilePath)
-        logging.info("Clip has been saved locally")
+        ## If we're on the last subclip
+        if a + 1 == chunk_count:
+            subclipDurationSeconds = clip.duration - (a * chunk_length_secs)
+        else:
+            subclipDurationSeconds = chunk_length_secs
+            
+        startSeconds = chunk_length_secs * a
+        
+        fileOutPath = "/tmp/" + subclipFileName
+        
+        # ff = ffmpy.FFmpeg(
+        #     executable="./ffmpeg",
+        #     inputs={
+        #             tempClipFilePath : f"-ss {startSeconds}"
+        #             },
+        #     outputs={
+        #             fileOutPath : f"-t {subclipDurationSeconds} -c copy"
+        #             }
+        # )
+        # logging.info(f"ff.cmd: {ff.cmd}")
+        # ff.run()
+
+        # ffmpegCommand = f'./ffmpeg -ss {startSeconds} -i "{tempClipFilePath}" -t {subclipDurationSeconds} -c copy -bsf:a aac_adtstoasc "{fileOutPath}"'
+        ffmpegCommand = f'./ffmpeg -ss {startSeconds} -i "{tempClipFilePath}" -t {subclipDurationSeconds} -bsf:a aac_adtstoasc -acodec copy -vcodec copy "{fileOutPath}"'
+        logging.info(f"ffmpegCommand: {ffmpegCommand}")
+        # p = subprocess.Popen(ffmpegCommand)
+        # p.wait()
+        result = os.popen(ffmpegCommand).read()
+        logging.info("command run")
+        logging.info(f"result: {result}")
             
         bbs.create_blob_from_path(
             container_name=container,
-            blob_name=f"{filePrefix}_{fileName}",
-            file_path=tempFilePath
+            blob_name=subclipFileName,
+            file_path=fileOutPath
         )
         B = datetime.now()
-        logging.info(f"Uploaded, time taken: {B-A}")
+        logging.info(f"{subclipPrefix} uploaded, time taken: {B-A}")
 
     return f"{chunk_count} files uploaded to the `{container}` container"
